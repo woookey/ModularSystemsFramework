4987f9a632b230cee7aacbcade6fd566218f09d6
woookey <woo.barczyk@gmail.com>
2017-06-01 23:46:07 +0100

    create system signal space header

---
+++ src/system/systemSignals.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_SIGNALS_H
+#define SYSTEM_SIGNALS_H
+
+#include <RF_events.h>
+
+typedef enum systemSignalSpace
+{
+	SYSTEM_SIGNAL_NUMBER_OF_SIGNALS = RF_LAST_SIGNAL
+} systemSignalSpace;
+
+#endif
