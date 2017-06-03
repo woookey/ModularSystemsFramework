d6fcef5b45c8111c7993b173934b047cc58cd127
woookey <woo.barczyk@gmail.com>
2017-06-03 15:06:22 +0100

    create porting files for stm32f4 and synthetic

---
+++ src/porting/stm32f4/porting.c
@@ -0,0 +1,7 @@
+#include <RF_porting.h>
+
+/**
+ * TODO: implement disabling/enabling interrupts
+ */
+void RF_enterCriticalSection() {}
+void RF_exitCriticalSection() {}
---
+++ src/porting/synthetic/porting.c
@@ -0,0 +1,8 @@
+#include <RF_porting.h>
+
+/**
+ * TODO: no implementation will only be working on
+ * no interrupts/threads applications
+ */
+void RF_enterCriticalSection() {}
+void RF_exitCriticalSection() {}
