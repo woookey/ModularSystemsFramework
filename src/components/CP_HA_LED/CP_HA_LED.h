3b69981b66bfe38e85afe1f5e00cfd50cb24a8f2
wookie <woo.barczyk@gmail.com>
2017-11-16 17:51:02 +0000

    Synthetic and target for HA_LED to be written... it is not yet implemented

--- src/components/CP_HA_LED/CP_HA_LED.h
+++ src/components/CP_HA_LED/CP_HA_LED.h
@@ -4,6 +4,12 @@
 typedef struct LEDType LEDType_t;
 typedef LEDType_t *const CP_HA_LEDStruct;
 
+CP_HA_LEDStruct CPUActivityLED;
+
+CP_HA_LEDStruct powerOnIndicationLED;
+CP_HA_LEDStruct executingIndicationLED;
+CP_HA_LEDStruct safetyStopIndicationLED;
+
 void CP_HA_LEDConstruct(CP_HA_LEDStruct LEDInstance);
 void CP_HA_LEDSwitchOn(CP_HA_LEDStruct LEDInstance);
 void CP_HA_LEDSwitchOff(CP_HA_LEDStruct LEDInstance);
