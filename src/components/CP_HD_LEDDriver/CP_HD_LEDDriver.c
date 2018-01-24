53825cdecd5e0ec905412c840ce0d508c744772e
wookie <woo.barczyk@gmail.com>
2018-01-24 11:48:49 +0000

    implement CP_HD_LedDriver in general form

--- src/components/CP_HD_LEDDriver/CP_HD_LEDDriver.c
+++ src/components/CP_HD_LEDDriver/CP_HD_LEDDriver.c
@@ -1,4 +1,77 @@
 #include <CP_HD_LEDDriver.h>
+#include <CP_HA_LED.h>
+#include <stdbool.h>
 
-void CP_HD_LED_switchLEDOff() {}
-void CP_HD_LED_switchLEDOn() {}
+/**
+ * Definition of a reference to a LED structures
+ */
+struct LEDStructure
+{
+	CP_HA_LEDStruct led;
+	bool isInitialised;
+	bool isOn;
+};
+
+/**
+ * Static definitions
+ */
+static struct LEDStructure CPUActivityLED_t =
+{
+		.led = &CPUActivityLED,
+		.isInitialised = false,
+		.isOn = false,
+};
+
+static struct LEDStructure powerOnIndicationLED_t =
+{
+		.led = &powerOnIndicationLED,
+		.isInitialised = false,
+		.isOn = false,
+};
+
+static struct LEDStructure executingIndicationLED_t =
+{
+		.led = &executingIndicationLED,
+		.isInitialised = false,
+		.isOn = false,
+};
+
+static struct LEDStructure safeyStopIndicationLED_t =
+{
+		.led = &safetyStopIndicationLED,
+		.isInitialised = false,
+		.isOn = false,
+};
+
+LEDReference CP_HD_CPUActivityLED = &CPUActivityLED_t;
+LEDReference CP_HD_powerOnIndicationLED = &powerOnIndicationLED_t;
+LEDReference CP_HD_executingIndicationLED = &executingIndicationLED_t;
+LEDReference CP_HD_safetyStopIndicationLED = &safeyStopIndicationLED_t;
+
+
+void CP_HD_LED_initialiseLED(LEDReference ledRef)
+{
+	if (!ledRef->isInitialised)
+	{
+		CP_HA_LEDConstruct(ledRef->led);
+	}
+	ledRef->isInitialised = true;
+}
+
+void CP_HD_LED_switchLEDOff(LEDReference ledRef)
+{
+	if (ledRef->isOn)
+	{
+		CP_HA_LEDSwitchON(ledRef->led);
+		ledRef->isOn = true;
+	}
+}
+
+void CP_HD_LED_switchLEDOn(LEDReference ledRef)
+{
+	if (!ledRef->isOn)
+	{
+		CP_HA_LEDSwitchOff(ledRef->led);
+		ledRef->isOn = false;
+	}
+}
