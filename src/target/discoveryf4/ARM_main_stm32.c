6c5adb6b195ea1bef7b3e4e7e6f33f5d992a8323
wookie <woo.barczyk@gmail.com>
2018-03-17 20:24:01 +0000

    Remove sample_target from the build - it is no longer used

--- src/target/discoveryf4/ARM_main_stm32.c
+++ src/target/discoveryf4/ARM_main_stm32.c
@@ -1,5 +1,5 @@
-#include <stm32f4xx.h>
 #include <LEDManager.h>
+#include <CP_HA_HardwareSetup.h>
 #include <RF_events.h>
 #include <RF_scheduler.h>
 #include <RF_dispatcher.h>
@@ -7,19 +7,10 @@
 #include <systemSignals.h>
 
 #include <stdint.h>
-#include <stm32f4xx_hal_conf.h>
-#include <stm32f4xx_hal_rcc.h>
-#include <stm32f4xx_hal_rcc_ex.h>
-#include <stm32f4xx_hal.h>
 #include <assert.h>
-
-static void setupHardware(void);
-static void initClocks(void);
 
 int main()
 {
-	SystemInit();
-	SystemCoreClockUpdate();
 	setupHardware();
 	RFEvent LEDManagerPool[10];
 	RF_DispatcherCtor();
@@ -35,53 +26,4 @@ int main()
 		runScheduler();
 	}
 	return 0;
-}
-
-void setupHardware(void)
-{
-	initClocks();
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-}
-
-static void initClocks()
-{
-	// TODO: Verify the speed of clock
-    HAL_RCC_DeInit();
-    RCC_OscInitTypeDef rccOscInstance =
-    {
-    		.OscillatorType = RCC_OSCILLATORTYPE_HSE,
-			.HSEState = RCC_HSE_ON,
-			.LSEState = RCC_LSE_OFF,
-			.HSIState = RCC_HSI_OFF,
-			.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT,
-			.LSIState = RCC_LSI_OFF,
-			.PLL =
-			{
-					.PLLState = RCC_PLL_ON,
-					.PLLSource = RCC_PLLSOURCE_HSE,
-					.PLLM = 8,
-					.PLLN = 336,
-					.PLLP = RCC_PLLP_DIV2,
-					.PLLQ = 7,
-			},
-    };
-    assert(HAL_RCC_OscConfig(&rccOscInstance) == HAL_OK);
-
-    RCC_ClkInitTypeDef rccClkInstance =
-    {
-    		  .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK,
-    		  .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
-    		  .AHBCLKDivider = RCC_SYSCLK_DIV1,
-    		  .APB1CLKDivider = RCC_HCLK_DIV4,
-    		  .APB2CLKDivider = RCC_HCLK_DIV2,
-    };
-
-    assert(HAL_RCC_ClockConfig(&rccClkInstance, FLASH_ACR_LATENCY_5WS) == HAL_OK);
-}
-
-void SysTick_Handler(void)
-{
-	HAL_IncTick();
-	RFTimer_decreaseTimersByOneTick();
 }
