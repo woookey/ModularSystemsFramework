afb5f1a97ed167d257ca4dd319bf521bf8e95f92
wookie <woo.barczyk@gmail.com>
2018-03-17 17:27:12 +0000

    Improve synthetic printing for LEDs

--- src/components/CP_HA_LED/synthetic/CP_HA_LED.c
+++ src/components/CP_HA_LED/synthetic/CP_HA_LED.c
@@ -1,16 +1,17 @@
 #include <CP_HA_LED.h>
 #include <LEDType.h>
 #include <stdio.h>
+#include <string.h>
 
 #define CPU_ACTIVITY_LED_PORT 12
-#define POWER_ON_INDICATION_LED_PORT 13
+#define POWER_ON_INDICATION_LED_PORT 15
 #define EXECUTING_INDICATION_LED_PORT 14
 #define SAFETY_STOP_INDICATION_LED_PORT 15
 
-static struct LEDType CPUActivityLEDInstance = { .pin = (uint8_t)CPU_ACTIVITY_LED_PORT };
-static struct LEDType powerOnIndicationLEDInstance = { .pin = (uint8_t)POWER_ON_INDICATION_LED_PORT };
-static struct LEDType executingIndicationLEDInstance = { .pin = (uint8_t)EXECUTING_INDICATION_LED_PORT };
-static struct LEDType safetyStopIndicationLEDInstance = { .pin = (uint8_t)SAFETY_STOP_INDICATION_LED_PORT };
+static struct LEDType CPUActivityLEDInstance = { .pin = (uint8_t)CPU_ACTIVITY_LED_PORT, .port = 'D' };
+static struct LEDType powerOnIndicationLEDInstance = { .pin = (uint8_t)POWER_ON_INDICATION_LED_PORT, .port = 'C' };
+static struct LEDType executingIndicationLEDInstance = { .pin = (uint8_t)EXECUTING_INDICATION_LED_PORT, .port = 'D' };
+static struct LEDType safetyStopIndicationLEDInstance = { .pin = (uint8_t)SAFETY_STOP_INDICATION_LED_PORT, .port = 'D' };
 
 
 CP_HA_LEDStruct CPUActivityLED = &CPUActivityLEDInstance;
@@ -20,15 +21,15 @@ CP_HA_LEDStruct safetyStopIndicationLED = &safetyStopIndicationLEDInstance;
 
 void CP_HA_LEDConstruct(CP_HA_LEDStruct LEDInstance)
 {
-	printf("LED connected to port %d constructed\n", LEDInstance->pin);
+	printf("LED connected to port %c%d constructed\n", LEDInstance->port, LEDInstance->pin);
 }
 
 void CP_HA_LEDSwitchOn(CP_HA_LEDStruct LEDInstance)
 {
-	printf("LED connected to port %d switched on\n", LEDInstance->pin);
+	printf("LED connected to port %c%d switched on\n", LEDInstance->port, LEDInstance->pin);
 }
 
 void CP_HA_LEDSwitchOff(CP_HA_LEDStruct LEDInstance)
 {
-	printf("LED connected to port %d switched off\n", LEDInstance->pin);
+	printf("LED connected to port %c%d switched off\n", LEDInstance->port, LEDInstance->pin);
 }
