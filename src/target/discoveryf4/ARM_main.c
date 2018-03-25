bbf212b9e4c4c27aadd109b67f04f776d416eb6b
wookie <woo.barczyk@gmail.com>
2018-03-25 15:18:16 +0100

    Add constructing power manager in the main file

--- src/target/discoveryf4/ARM_main.c
+++ src/target/discoveryf4/ARM_main.c
@@ -1,4 +1,5 @@
 #include <LEDManager.h>
+#include <CP_BH_PowerManager.h>
 #include <CP_HA_HardwareSetup.h>
 #include <RF_events.h>
 #include <RF_scheduler.h>
@@ -11,13 +12,16 @@ int main()
 {
 	setupHardware();
 	RFEvent LEDManagerPool[10];
+	RFEvent PowerManagerPool[10];
 	RF_DispatcherCtor();
 
 
 	startAgent(LEDManager, &LEDManagerConstructor, AGENT_PRIORITY_0,
 						&LEDManagerPool[0], sizeof(RFEvent)*10);
+	startAgent(PowerManager, &PowerManagerConstructor, AGENT_PRIORITY_0,
+						&PowerManagerPool[0], sizeof(RFEvent)*10);
 
-	RF_Dispatcher_RegisterNumberOfAgents(1);
+	RF_Dispatcher_RegisterNumberOfAgents(2);
 	RF_Dispatcher_RegisterNumberOfEvents(SYSTEM_SIGNAL_NUMBER_OF_SIGNALS);
 	startHardware();
 	/**
